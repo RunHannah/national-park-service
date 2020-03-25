ccce1c2802a5059e354e618add7ff146135613c3
Hannah Lee <leexhannah@gmail.com>
2020-03-24 21:51:03 -0700

    update get request for blogData and parks

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -14,20 +14,37 @@ class Landing extends Component {
     this.state = {
       data: [],
       stateValue: '',
-      isLoading: true
+      isLoading: true,
+      blogData: []
     };
   }
-  callApi = async state => {
-    const response = await fetch(`http://localhost:5000?state=${state}`);
-    const body = await response.json();
 
-    if (response.status !== 200) throw Error(body.message);
+  async componentDidMount() {
+    await fetch('http://localhost:5000')
+      .then(response => response.json())
+      .then(data => {
+        console.log('data', data);
+        this.setState({ blogData: data });
+      });
 
-    this.setState({
-      data: body.data,
-      loading: false
-    });
-  };
+    await fetch('http://localhost:5000/parks')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ data: data.data });
+      });
+  }
+
+  // callApi = async state => {
+  //   const response = await fetch(`http://localhost:5000?state=${state}`);
+  //   const body = await response.json();
+
+  //   if (response.status !== 200) throw Error(body.message);
+
+  //   this.setState({
+  //     data: body.data,
+  //     loading: false
+  //   });
+  // };
 
   selectState = state => {
     this.setState({ stateValue: state });
@@ -35,6 +52,8 @@ class Landing extends Component {
   };
 
   render() {
+    const { blogData } = this.state;
+
     if (this.state.loading === false) {
       let { data } = this.state;
 
@@ -46,12 +65,14 @@ class Landing extends Component {
       );
     }
 
-    return (
-      <div style={landingStyle}>
-        <HomepageHero selectState={this.selectState} />
-        <ArticleContent />
-      </div>
-    );
+    if (blogData) {
+      return (
+        <div style={landingStyle}>
+          <HomepageHero selectState={this.selectState} />
+          <ArticleContent blogData={blogData} />
+        </div>
+      );
+    }
   }
 }
 
--- server.js
+++ server.js
@@ -3,21 +3,37 @@ const app = express();
 const axios = require('axios');
 const config = require('./config');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/npsBlog', (err, client) => {
+  if (err) throw err;
+  const db = client.db('npsBlog');
+  const collection = db.collection('posts');
+  app.locals.collection = collection;
+});
 
 app.use(cors());
 
 app.get('/', async (req, res) => {
-  const { state } = req.query;
-  console.log('state', state);
+  const collection = req.app.locals.collection;
 
+  collection
+    .find({})
+    .toArray()
+    .then(response => res.json(response));
+
+  res.send({ collectionData: collection });
+});
+
+app.get('/parks', async (req, res) => {
   try {
     await axios
-      .get(
-        `${config.BASE_URL}/${config.BASE_PARAMS}${state}&api_key=${config.API_KEY}`
-      )
+      .get(`${config.BASE_URL}/${config.BASE_PARAMS}&api_key=${config.API_KEY}`)
       .then(response => {
         res.json(response.data);
       });
+
+    console.log('server parks', response.data);
   } catch (error) {
     console.log(error);
   }
