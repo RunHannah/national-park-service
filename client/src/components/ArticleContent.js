64092253ed6ceb84598485b165692a106b95ee96
Hannah Lee <leexhannah@gmail.com>
2020-03-26 22:51:33 -0700

    display and style blog posts

--- client/src/components/ArticleContent.js
+++ client/src/components/ArticleContent.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './ArticleContent.css';
 
-function MainContent() {
-  return <div>Main Content Blogs</div>;
+function MainContent({ blogData }) {
+  console.log('blogData', blogData);
+
+  return (
+    <div className='blogContainer'>
+      <div className='blogList'>
+        {blogData
+          .sort((a, b) => a.order - b.order)
+          .map(post => (
+            <div className='post' key={post._id}>
+              <img className='postImg' src={post.imageSrc} alt={post.alt} />
+              <h3 className='postTitle'>{post.featureTitle}</h3>
+              <p className='postDescription'>{post.featureDescription}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default MainContent;
