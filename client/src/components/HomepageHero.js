3742eb2045c833eae378031860a3e095275653c0
Hannah Lee <leexhannah@gmail.com>
2020-03-27 22:53:33 -0700

    update favicon and homepageHero styling

--- client/src/components/HomepageHero.js
+++ client/src/components/HomepageHero.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import SearchForm from './SearchForm';
-
-const homepageStyle = {
-  height: '500px',
-  width: '100%',
-  backgroundPosition: '50%',
-  backgroundSize: 'cover',
-  position: 'relative',
-  backgroundRepeat: 'no-repeat',
-  backgroundImage:
-    'url(https://www.nps.gov/common/uploads/hero_home/pwr/promo/56ED3EB8-F05E-2155-F4563FA4940DDC16.jpg?width=2400&height=700&mode=crop&quality=90)'
-};
+import './HomepageHero.css';
 
 function HomepageHero(props) {
   return (
-    <div style={homepageStyle}>
+    <div className='homepageStyle'>
       <SearchForm selectState={props.selectState} />
     </div>
   );
