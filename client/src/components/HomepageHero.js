a36ad0e9ec6663a9610b178ca09d5d147cad4631
Hannah Lee <leexhannah@gmail.com>
2020-03-28 15:47:52 -0700

    display homepage content title

--- client/src/components/HomepageHero.js
+++ client/src/components/HomepageHero.js
@@ -4,8 +4,19 @@ import './HomepageHero.css';
 
 function HomepageHero(props) {
   return (
-    <div className='homepageStyle'>
-      <SearchForm selectState={props.selectState} />
+    <div className='homepageContainer'>
+      <div className='homepageWrapper'>
+        <div className='contentWrapper'>
+          <div className='content'>
+            <span className='contentTitle'>
+              COMMEMORATING WOMEN IN CIVIC ENGAGEMENT
+            </span>
+            <h2 className='contentSubtitle'>Women's History Month</h2>
+            <a className='contentButton'>Explore History</a>
+          </div>
+        </div>
+        <SearchForm selectState={props.selectState} />
+      </div>
     </div>
   );
 }
