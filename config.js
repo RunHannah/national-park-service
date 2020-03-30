4a556aefdb590923973575059016d87a86300e54
Hannah Lee <leexhannah@gmail.com>
2020-03-29 20:07:47 -0700

    remove state lookup in api request

--- config.js
+++ config.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   API_KEY: process.env.API_KEY,
-  BASE_URL: 'https://developer.nps.gov/api/v1',
-  BASE_PARAMS: 'parks?stateCode='
+  BASE_URL: 'https://developer.nps.gov/api/v1/parks'
 };
+
+// module.exports = {
+//   API_KEY: process.env.API_KEY,
+//   BASE_URL: 'https://developer.nps.gov/api/v1/parks',
+//   BASE_PARAMS: 'parks?stateCode='
+// };
