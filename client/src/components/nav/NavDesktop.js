50b45830ae143adaafef40f738af2bab99412a4b
Hannah Lee <leexhannah@gmail.com>
2020-04-11 20:01:47 -0700

    display image for desktop nav

--- client/src/components/nav/NavDesktop.js
+++ client/src/components/nav/NavDesktop.js
@@ -4,31 +4,43 @@ import './NavDesktop.css';
 
 class NavDesktop extends Component {
   state = {
-    menuList: menuList
+    menuList: menuList,
   };
 
   render() {
     return (
       <div className='navDesktopContainer'>
         <ul className='navDesktopList'>
-          {this.state.menuList.map(obj => (
-            <li className={`list ${obj.id}`} key={obj.id}>
-              <a href=''>
-                <span>{obj.section}</span>
-              </a>
-              <ul className='items'>
-                {obj.items.map(sub => {
-                  return (
-                    <li class='item' key={sub.service}>
-                      <a href=''>
-                        <span>{sub.service}</span>
-                      </a>
+          {this.state.menuList.map((obj) => {
+            if (obj.id === 'searchForParks') {
+              return (
+                <li className={`list ${obj.id}`} key={obj.id}>
+                  <img src='https://www.nps.gov/common/commonspot/templates/assetsCT/images/globalnav/GlobalNavPromo_GLAC_JacobWFrank_158x94.jpg'></img>
+                  <ul className='items'>
+                    <li className='itemDesktop' key={obj.items[0].service}>
+                      <span>{obj.items[0].service}</span>
                     </li>
-                  );
-                })}
-              </ul>
-            </li>
-          ))}
+                  </ul>
+                </li>
+              );
+            } else {
+              return (
+                <li className={`list ${obj.id}`} key={obj.id}>
+                  <span>{obj.name}</span>
+
+                  <ul className='items'>
+                    {obj.items.map((sub) => {
+                      return (
+                        <li className='itemDesktop' key={sub.service}>
+                          <span>{sub.service}</span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </li>
+              );
+            }
+          })}
         </ul>
       </div>
     );
