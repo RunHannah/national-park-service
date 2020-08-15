16bad5d1c711dc4a87573d5c14ca770d6ce4ba30
Hannah Lee <leexhannah@gmail.com>
2020-08-14 23:22:03 -0700

    uninstall mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
